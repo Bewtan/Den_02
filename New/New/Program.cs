460ac80969f5cfcaea8a2ce70a25530d5c37a3da
Bewtan <g4e6o15r7g4i9@gmail.com>
2019-02-01 17:02:11 +0200

    problem

---
+++ New/New/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cen = int.Parse(Console.ReadLine());
+
+            Console.Write("years: " + cen * 1000 + "\ndays: " + cen * 1000 * 365 + "\nhours: " + cen * 1000 * 365 * 24 + "\nmin: " + cen * 1000 * 365 * 24* 60);
+        }
+    }
+}
